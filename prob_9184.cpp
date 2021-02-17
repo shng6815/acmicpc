77127d2ec9be9f175f1ee556a0f9c998ab6fb9c4
서20 <shng6815@gmail.com>
2021-02-17 13:53:00 +0000

    최종 수정

--- prob_9184.cpp
+++ prob_9184.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
 int dp[21][21][21];
+int a,b,c;
 
 int solv(int a,int b,int c){
 	if((a<=0||b<=0)||c<=0)
@@ -9,6 +9,9 @@ int solv(int a,int b,int c){
 	if(dp[a][b][c]!=0){
 		return dp[a][b][c];
 	}
+	if ((a>20||b>20)||c>20)
+    	return w(20, 20, 20);
+
 	if(a<b&&b<c){
 		dp[a][b][c] = solv(a,b,c-1)+solv(a,b-1,c-1)-solv(a,b-1,c);
 		return dp[a][b][c];
@@ -20,16 +23,11 @@ int solv(int a,int b,int c){
 }
 
 int main() {
-	int a,b,c;
 	for(;;){
 		cin>>a>>b>>c;
 		if((a==-1&&b==-1)&&c==-1)
 			break;
-		if((a>20||b>20)||c>20){
-			printf("w(20, 20, 20) = %d",solv(20,20,20));
-			continue;
-		}
-		printf("w(%d, %d, %d) = %d",a,b,c,solv(a,b,c));
+		printf("w(%d, %d, %d) = %d\n",a,b,c,solv(a,b,c));
 	}
 	
 } 
